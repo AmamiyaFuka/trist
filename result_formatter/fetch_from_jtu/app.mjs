61e7663acadb9e4b86edebec2ae7c8497cd59255
AmamiyaFuka <amamiya.fuka@gmail.com>
2024-09-13 22:49:06 +0900

    村上国際2023追加

--- result_formatter/fetch_from_jtu/app.mjs
+++ result_formatter/fetch_from_jtu/app.mjs
@@ -7,26 +7,42 @@ import Formatter from './formatter.mjs';
 const formatter = new Formatter();
 
 // 今はハードコーティング
-const course_id = {
-	"name": "第38回2024 トライアスロン伊良湖大会 B",
-	"starttime": new Date('2024-09-08T10:00+09:00').getTime(),
-	"weather": "晴れ",
-	"distance": {
-		"swim": 1.5,
-		"bike": 42.0,
-		"run": 10.0
+const minami_hokkaido = {
+	course: {
+		name: 'アイアンマンジャパン みなみ北海道',
+		short_name: 'IMジャパン2024',
+		starttime: new Date('2024-09-15T06:30' + '+09:00').getTime(),
+		weather: '',
+		distance: {
+			swim: 3.8,
+			bike: 180,
+			run: 42.2,
+		},
+		locale: '日本, 北海道',
+		url: 'https://triathlon-south-hokkaido.com/archives/3104',
 	},
-	"locale": "日本, 愛知県",
-	"category": "standard",
-	"url": "https://www.irago-triathlon.jp/",
-};
-// JTUのリザルトページURLのprogram_id
-const result_id = '282_2';
-const output_file = 'irako_2024_b';
+	result_id: 'xxx_x', output_file: 'minami-hokkaido_2024'};
+	
+const murakami_2023 = {
+	course: {
+		name: '2023村上・笹川流れ国際トライアスロン大会',
+		short_name: '村上2023',
+		starttime: new Date('2023-09-24T09:20' + '+09:00').getTime(),
+		weather: '晴れ',
+		distance: {
+			swim: 1.5,
+			bike: 40,
+			run: 10,
+		},
+		locale: '日本, 新潟県',
+		url: 'https://www.jtu.or.jp/wordpress/wp-content/uploads/2024/01/event.report_murakami2023.pdf',
+	},
+	result_id: '225_1', output_file: 'murakami_2023'};
+
+const target = murakami_2023;
 
 Promise.all([
-	Course.get(course_id),
-	Result.get(result_id).then(json => formatter.to_trist_from_jtu(json))
-])
-	.then(([course, result]) => fs.writeFile(output_file + '.json', JSON.stringify({ course, result })));
+	Course.get(target.course),
+	Result.get(target.result_id).then(json => formatter.to_trist_from_jtu(json))
+]).then(([course, result]) => fs.writeFile('./out/' + target.output_file + '.json', JSON.stringify({ course, result })));
 
--- result_formatter/fetch_from_jtu/course.mjs
+++ result_formatter/fetch_from_jtu/course.mjs
@@ -1,4 +1,14 @@
+const get_category = ({swim, bike, run}) => {
+	if (swim >= 3.0 && bike >= 91 && run >= 22) return 'long';
+	if (swim >= 1.999 && bike >= 80 && run >= 20) return 'middle';
+	if (swim >= 1 && bike >= 35 && run >= 8) return 'standard';
+	if (swim > 0.5 && bike > 13 && run > 3.5) return 'sprint';
+	if (swim <= 0.5 && bike <= 13 && run <= 3.5) return 'super-sprint';
+	throw new Error('Unknown category');
+};
+
 const get = id => {
+	if (!id.category) id.category = get_category(id.distance);
 	return (async () => id)();
 };
 
